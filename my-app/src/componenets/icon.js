0ce648cfa09bc20adcc63f5c48bcaa47a003763d
Tanay <tanaydwivedi456@gmail.com>
2021-06-12 13:31:18 +0530

    react-app started

---
+++ my-app/src/componenets/icon.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {FaTimes,FaPen,FaRegCircle} from 'react-icons'
+const Icon=( {name} )=>{
+    return(
+        <h1><FaTimes className="icon"/>
+
+        </h1>
+    );
+};
+
+export default Icon;
