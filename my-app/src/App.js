fabacd06ce02ac8dec34ed3c32bc365ff202db55
Tanay <tanaydwivedi456@gmail.com>
2021-06-14 19:34:26 +0530

    style:added bootstrap and toastify

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,31 @@
+import React,{useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 import Icon from "./components/Icon"
-function App() {
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Card,CardBody,Container,Button,Col,Row} from "reactstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const itemArray=new Array(9).fill("empty")
+
+const App=() => {
+  const [isCross,setIsCross]= useState(false)
+  const [winMessage,setWinMessage]=useState("")
+
+
+  const reloadGame=()=>{
+
+  }
+
+  const checkIsWinner=()=>{
+
+  }
+
+  const changeItem=itemNumber=>{
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
